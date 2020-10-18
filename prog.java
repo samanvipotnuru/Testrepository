9754588b4a50377cf52bd3f83a1b1f5755a04d9f
samanvipotnuru <60637608+samanvipotnuru@users.noreply.github.com>
2020-10-18 17:07:14 +0530

    Add files via upload

---
+++ prog.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class A
+{
+public static void main(String[] args)
+{
+int a[]={10,10,10};
+System.out.println("The elements of the array are:");
+for(int i=0;i<3;i++)
+{
+System.out.println(a[i]);
+}
+
+}
+}
